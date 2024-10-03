07df1693a7238e4cd7f24cc01883038bf37cef91
Ahmed <atkatary@gmail.com>
2024-10-03 03:54:36 +0000

    adding get_masks and show_mask

--- backend/api/sam.py
+++ backend/api/sam.py
@@ -1,14 +1,22 @@
 import torch
+import numpy as np
 from PIL import Image
-import requests
+import matplotlib.pyplot as plt
 from transformers import SamModel, SamProcessor
 
 def get_masks(img, boxes, device="cuda"):
+    if len(boxes[0]) == 0: return 
+
     model_id = "facebook/sam-vit-huge"
     processor = SamProcessor.from_pretrained(model_id)
     model = SamModel.from_pretrained(model_id).to(device)
 
-    processed_img = processor(img, input_points=boxes, return_tensors="pt").to(device)
+    processed_img = processor(img, input_boxes=[boxes], return_tensors="pt").to(device)
+    img_embeddings = model.get_image_embeddings(processed_img["pixel_values"])
+    
+    processed_img.pop("pixel_values", None)
+    processed_img.update({"image_embeddings": img_embeddings})
+
     with torch.no_grad(): prediction = model(**processed_img)
 
     masks = processor.image_processor.post_process_masks(
@@ -20,10 +28,28 @@ def get_masks(img, boxes, device="cuda"):
 
     return {'masks': masks, 'scores': scores}
 
+def show_mask(mask, ax, random_color=False):
+    if random_color:
+        color = np.concatenate([np.random.random(3), np.array([0.6])], axis=0)
+    else:
+        color = np.array([30/255, 144/255, 255/255, 0.6])
+        
+    h, w = mask.shape[-2:]
+    mask_image = mask.reshape(h, w, 1) * color.reshape(1, 1, -1)
+    ax.imshow(mask_image)
 
+def show_masks_on_image(raw_image, masks, scores):
+    if len(masks.shape) == 4: masks = masks.squeeze()
+    if scores.shape[0] == 1: scores = scores.squeeze()
 
-    
+    nb_predictions = scores.shape[-1]
+    fig, axes = plt.subplots(1, nb_predictions, figsize=(15, 15))
 
-    
+    for i, (mask, score) in enumerate(zip(masks, scores)):
+      mask = mask.cpu().detach()
+      axes[i].imshow(np.array(raw_image))
 
-
+      show_mask(mask, axes[i])
+      axes[i].title.set_text(f"Mask {i+1}, Score: {score.item():.3f}")
+      axes[i].axis("off")
+    plt.show()
