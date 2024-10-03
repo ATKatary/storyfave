f57d4ffdbdd7c34c45ea5d60ab74dde5b6ebd258
Ahmed <atkatary@gmail.com>
2024-10-03 04:45:43 +0000

    adding get_masks and show_mask

--- backend/utils.py
+++ backend/utils.py
@@ -1,4 +1,5 @@
 import math
+import numpy as np
 from pathlib import Path
 from matplotlib import pyplot as plt 
 from transformers import CLIPTokenizer
@@ -9,15 +10,21 @@ default_tokenizer = CLIPTokenizer.from_pretrained(SDV1_5, subfolder="tokenizer")
 
 def display(imgs, r, c=-1, w=3, h=3):
     n = len(imgs)
-    if c == -1: c = math.ceil(r / n)
+    if c == -1: c = math.ceil(n / r)
 
-    fig = plt.figure(figsize=(w*c, h)) 
-    fig.add_subplot(r, c, 1)
-    fig.patch.set_visible(False)
+    fig, axes = plt.subplots(r, c, figsize=(w*c, h))
 
-    for i in range(n):
-        fig.add_subplot(r, c, i + 1) 
-        plt.imshow(imgs[i]['img']) 
-        plt.axis("tight")
-        plt.title(imgs[i]['label'])
+    for i in range(r):
+      for j in range(c):
+        if r > 1: 
+          if c > 1: axe = axes[i][j]; z = i*r + j
+          else: axe = axes[i]; z = i
+        elif c > 1: axe = axes[j]; z = j
+        else: axe = axes; z = 0
+  
+        if z < n:
+          axe.imshow(np.array(imgs[z]['img']))
+          axe.title.set_text((imgs[z]['label']))
+          axe.axis("off")
+    plt.show()
 
