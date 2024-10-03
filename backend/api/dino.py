2df6baa5617ef73e0b53511fdb38c084bd466182
Ahmed <atkatary@gmail.com>
2024-10-03 01:51:35 +0000

    adding get_bounding_boxes and get_masks

--- backend/api/dino.py
+++ backend/api/dino.py
@@ -5,22 +5,30 @@ from transformers import AutoProcessor, AutoModelForZeroShotObjectDetection
 import supervision as sv
 from inference.models.yolo_world.yolo_world import YOLOWorld
 
+yolo = YOLOWorld(model_id="yolo_world/l")
+
+model_id = "IDEA-Research/grounding-dino-tiny"
+dino_processor = AutoProcessor.from_pretrained(model_id)
+dino = AutoModelForZeroShotObjectDetection.from_pretrained(model_id)
+
 def get_bounding_boxes(img, prompt, model="dino", device="cuda"):
     if model == "dino":
-        model_id = "IDEA-Research/grounding-dino-tiny"
-        processor = AutoProcessor.from_pretrained(model_id)
-        model = AutoModelForZeroShotObjectDetection.from_pretrained(model_id).to(device)
+        dino.to(device)
+        processed_img = dino_processor(images=img, text=prompt, return_tensors="pt").to(device)
 
-        processed_img = processor(images=img, text=prompt, return_tensors="pt").to(device)
+        with torch.no_grad(): prediction = dino(**processed_img)
 
-        with torch.no_grad(): 
-            return model(**processed_img)
-        
+        return dino_processor.post_process_grounded_object_detection(
+            prediction,
+            processed_img.input_ids,
+
+            box_threshold=0.4,
+            text_threshold=0.3,
+            target_sizes=[img.size[::-1]]
+        )
     elif model == "yolo":
-        model = YOLOWorld(model_id="yolo_world/l")
-
-        model.set_classes(prompt.split(" "))
-        return model.infer(img)
+        yolo.set_classes(prompt.split(" "))
+        return yolo.infer(img)
 
     else: raise ValueError("invalid model")
 
